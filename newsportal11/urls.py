93a16a35031c71b6d877b9e6f540edca4f47e23c
shifakhan0755 <70809018+shifakhan0755@users.noreply.github.com>
2022-08-23 22:02:50 +0530

    Update urls.py

--- newsportal11/urls.py
+++ newsportal11/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from django.views.static import serve
+
 from home import views
 
 
@@ -64,5 +67,7 @@ urlpatterns = [
     path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
     path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
-
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root':  settings.STATIC_ROOT}),
+]
+urlpatterns=urlpatterns+static(settings.MEDIA_URL,decument_root=settings.MEDIA_ROOT)
